b6a0c7624aac42bbd69ae5722f510cfbe8a6362b
Rafe Sholler <rafe.sholler@gmail.com>
2025-07-30 19:06:37 -0400

    state machines and heavy boxes

---
+++ Player/idle.gd
@@ -0,0 +1,23 @@
+extends State
+
+func on_start(params: Array = []) -> void:
+	target.get_node("AnimatedSprite2D").play("idle")
+	target.velocity.x = 0
+	
+func update(delta: float) -> void:
+	pass
+
+func physics_update(delta: float) -> void:
+	target.move_and_slide()
+	
+	if not target.is_on_floor():
+		finished.emit(get_neighbor("Fall"))
+	
+	elif Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
+		finished.emit(get_neighbor("Run"))
+	
+	elif Input.is_action_pressed("jump"):
+		finished.emit(get_neighbor("Jump"))
+
+func on_end() -> void:
+	pass
---
+++ Player/jump.gd
@@ -0,0 +1,33 @@
+extends State
+
+func on_start(params: Array = []) -> void:
+	$JumpHoldTimer.start(.3)
+	
+func update(delta: float) -> void:
+	var dir = Input.get_axis("move_left", "move_right")
+	if dir == 0:
+		target.get_node("AnimatedSprite2D").play("idle")
+	else:
+		target.get_node("AnimatedSprite2D").play("run")
+
+func physics_update(delta: float) -> void:
+	if Input.is_action_pressed("jump") and not $JumpHoldTimer.is_stopped():
+		target.velocity.y = -100
+	
+	if not Input.is_action_pressed("jump"):
+		$JumpHoldTimer.stop()
+		finished.emit(get_neighbor("Fall"))
+	
+	var dir = Input.get_axis("move_left", "move_right")
+	target.velocity.x = target.speed * dir
+	
+	
+	
+	target.move_and_slide()
+
+func on_end() -> void:
+	$JumpHoldTimer.stop()
+
+
+func _on_jump_hold_timer_timeout() -> void:
+	finished.emit(get_neighbor("Fall"))
