18e71d52852ea0052ac730932b20713eb21fb41a
Rafe Sholler <rafe.sholler@gmail.com>
2025-07-30 16:47:15 -0400

    updated loopable and sprites

--- Loop/loopable.gd
+++ Loop/loopable.gd
@@ -1,7 +1,10 @@
 extends Node
 class_name Loopable
 
-@export var properties: Array[String]
+signal playback_started
+signal playback_ended
+
+@export var properties: Dictionary[Node, Array]
 @export var make_afterimage := false
 
 var is_looping = false
@@ -9,7 +12,9 @@ var is_looping = false
 
 func begin_playback() -> void:
 	is_looping = true
+	playback_started.emit()
 	
 
 func end_playback() -> void:
 	is_looping = false
+	playback_ended.emit()
