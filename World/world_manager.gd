44e8e67a6f0188d83a352ae4b8f7b621a56b0979
Rafe Sholler <rafe.sholler@gmail.com>
2025-07-31 17:55:05 -0400

    connected levels

--- World/world_manager.gd
+++ World/world_manager.gd
@@ -5,8 +5,11 @@ signal loaded_room #emits upon loading the next room, used for transitions
 signal load_next_room #emits upon transition end to load in next room
 
 var rooms := {
-	0: "res://Levels/OneLoop/ButtonPress1/button_press1.tscn",
-	1: "res://Levels/OneLoop/BoxPush1/box_push1.tscn"
+	0: "res://Levels/OneLoop/BoxPush1/box_push1.tscn",
+	1: "res://Levels/OneLoop/ButtonPress1/button_press1.tscn",
+	2: "res://Levels/OneLoop/BoxFreeze/box_freeze.tscn",
+	3: "res://Levels/OneLoop/TwoDoors/two_doors.tscn",
+	4: "res://Levels/OneLoop/MovingBox/moving_box.tscn",
 }
 
 var room_index: int = 0 :
