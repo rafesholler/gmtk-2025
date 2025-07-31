64ebf8b0f2349ab18acd50f80ab7682bbb91b41d
Rafe Sholler <rafe.sholler@gmail.com>
2025-07-31 14:53:42 -0400

    Added two doors level

---
+++ Levels/OneLoop/TwoDoors/two_doors.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+func _on_button_1_just_pressed() -> void:
+	$SlidePlatform1.activate()
+
+
+func _on_button_1_just_unpressed() -> void:
+	$SlidePlatform1.deactivate()
+
+
+func _on_button_2_just_pressed() -> void:
+	$SlidePlatform2.activate()
+
+
+func _on_button_2_just_unpressed() -> void:
+	$SlidePlatform2.deactivate()
