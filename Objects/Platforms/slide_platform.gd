388a3a56b0a5f65fbd2f90e03aea82a3615b7519
Rafe Sholler <rafe.sholler@gmail.com>
2025-08-01 22:11:47 -0400

    Bug fixes

--- Objects/Platforms/slide_platform.gd
+++ Objects/Platforms/slide_platform.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @export_custom(PROPERTY_HINT_RANGE, "0,.1,or_greater") var speed: float = .06
+@export var frame_delay: int = 0
+var frame_counter = 0
 
 var startpoint: Vector2
 var move_toward_end = false
@@ -10,19 +12,21 @@ func _ready() -> void:
 	startpoint = global_position
 
 func _physics_process(delta: float) -> void:
-	if move_toward_end:
-		global_position = lerp(global_position, $Endpoint.global_position, speed)
-	else:
-		global_position = lerp(global_position, startpoint, speed)
+	frame_counter += 1
+	if frame_delay == 0 or frame_counter % frame_delay == 0:
+		if move_toward_end:
+			global_position = lerp(global_position, $Endpoint.global_position, speed)
+		else:
+			global_position = lerp(global_position, startpoint, speed)
 
 func activate() -> void:
 	move_toward_end = true
-	#$Slide.pitch_scale = randf_range(1.0,1.5)
-	#$Slide.play()
+	$Slide.pitch_scale = randf_range(1.0,1.5)
+	$Slide.play()
 
 
 func deactivate() -> void:
-	#$Slide.pitch_scale = randf_range(1.0,1.5)
-	#$Slide.play()
+	$Slide.pitch_scale = randf_range(1.0,1.5)
+	$Slide.play()
 	move_toward_end = false
 	
