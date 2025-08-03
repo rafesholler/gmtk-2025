5840b83a0817cf39994b103dfc8f7d9d2f146df5
Gleb Syomichev <103398854+dirttop@users.noreply.github.com>
2025-08-02 23:22:54 -0400

    ui stuff

--- Levels/OneLoop/BoxPush1/box_push_1.gd
+++ Levels/OneLoop/BoxPush1/box_push_1.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _ready() -> void:
 	WorldManager.loaded_room.emit()
-	LoopManager.max_loops = 0
+	LoopManager.max_loops = 1
 
 func _on_trigger_body_entered(body: Node2D) -> void:
 	WorldManager.room_index = -1
