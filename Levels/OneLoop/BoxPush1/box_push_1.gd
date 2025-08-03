badb1b924bc242fc85d33bb8a8ee34538389071e
Gleb Syomichev <103398854+dirttop@users.noreply.github.com>
2025-08-02 23:06:50 -0400

    End Screen

--- Levels/OneLoop/BoxPush1/box_push_1.gd
+++ Levels/OneLoop/BoxPush1/box_push_1.gd
@@ -2,4 +2,10 @@ extends Node2D
 
 func _ready() -> void:
 	WorldManager.loaded_room.emit()
-	LoopManager.max_loops = 1
+	LoopManager.max_loops = 0
+
+func _on_trigger_body_entered(body: Node2D) -> void:
+	WorldManager.room_index = -1
+	var drop = get_tree().create_tween()
+	drop.set_trans(Tween.TRANS_SINE)
+	drop.tween_property($Title, "position", Vector2(180, 108), 2).set_ease(Tween.EASE_OUT)
